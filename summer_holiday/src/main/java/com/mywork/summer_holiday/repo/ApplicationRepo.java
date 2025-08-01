879ce298b518b591e9ea70b4e57f8e538463834c
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-01 10:13:31 +0530

    working on JPA

---
+++ summer_holiday/src/main/java/com/mywork/summer_holiday/repo/ApplicationRepo.java
@@ -0,0 +1,11 @@
+package com.mywork.summer_holiday.repo;
+
+import com.mywork.summer_holiday.entity.ApplicationEntity;
+
+public interface ApplicationRepo {
+    void savedApplication(ApplicationEntity entity);
+    ApplicationEntity findById(int id);
+    void updateApplication(int id, String newCompany);
+    void deleteById(int id);
+}
+
