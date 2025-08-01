cda0cc3b90cd0c4d02b1dc6d86109b5aea2f1167
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-01 10:13:48 +0530

    working on JPA

---
+++ summer_holiday/src/main/java/com/mywork/summer_holiday/service/ApplicationService.java
@@ -0,0 +1,10 @@
+package com.mywork.summer_holiday.service;
+
+import com.mywork.summer_holiday.entity.ApplicationEntity;
+
+public interface ApplicationService {
+    boolean validApplication(ApplicationEntity applicationEntity);
+    ApplicationEntity getApplicationById(int id);
+    void updateCompanyById(int id, String newCompany);
+    void removeById(int id);
+}
