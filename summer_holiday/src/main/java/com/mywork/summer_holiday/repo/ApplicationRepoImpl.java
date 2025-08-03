64219b270f3fdca87745229a5588e3b24b47ba94
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-03 17:29:05 +0530

    working on JPA

--- summer_holiday/src/main/java/com/mywork/summer_holiday/repo/ApplicationRepoImpl.java
+++ summer_holiday/src/main/java/com/mywork/summer_holiday/repo/ApplicationRepoImpl.java
@@ -79,4 +79,37 @@ public class ApplicationRepoImpl implements ApplicationRepo {
             em.close();
         }
     }
+
+    @Override
+    public ApplicationEntity findApplicationName(String name) {
+        EntityManager em = null;
+        ApplicationEntity entity = null;
+
+        try{
+            em = this.emf.createEntityManager();
+            entity = (ApplicationEntity) em.createNamedQuery("findApplicationName").setParameter("name", name).getSingleResult();
+            System.out.println("findApplicationName: "+entity);
+        }catch(PersistenceException e){
+            e.printStackTrace();
+        }finally {
+            if(em !=null) em.close();
+        }
+        return entity;
+    }
+
+    @Override
+    public ApplicationEntity findApplicationBySize(String appSize) {
+        EntityManager em = null;
+        ApplicationEntity entity = null;
+        try{
+            em = this.emf.createEntityManager();
+            entity = (ApplicationEntity) em.createNamedQuery("findApplicationBySize").setParameter("size", appSize).getSingleResult();
+            System.out.println("findApplicationBySize: "+entity);
+        }catch(PersistenceException e){
+            e.printStackTrace();
+        }finally {
+            if(em !=null) em.close();
+        }
+        return entity;
+    }
 }
